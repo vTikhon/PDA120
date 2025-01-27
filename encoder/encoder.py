7d675f27b64dfcde0194530c583341d9a90e9511
Tikhon Vergentev <tikhon.v@gmail.com>
2025-01-27 17:40:32 +0300

    changed encoder methods to static

--- encoder/encoder.py
+++ encoder/encoder.py
@@ -7,16 +7,19 @@ class Encoder:
     def __init__(self):
         pass
 
-    def bolean_one_column_encoder(self, df_feature, false_feature_parameter):
+    @staticmethod
+    def booleanOneColumnEncoder(df_feature, false_feature_parameter):
         df_feature_encoded = df_feature.apply(lambda x: 1 if x == false_feature_parameter else 0)
         return df_feature_encoded
 
-    def labelEncoder(self, df_feature):
+    @staticmethod
+    def labelEncoder(df_feature):
         label_encoder = LabelEncoder()
         df_feature_encoded = label_encoder.fit_transform(df_feature)
         return df_feature_encoded
 
-    def oneHotEncoder(self, df_feature):
+    @staticmethod
+    def oneHotEncoder(df_feature):
         # Применяем One-Hot Encoding для категориального признака
         one_hot_encoder = OneHotEncoder(sparse=False)  # sparse=False для получения DataFrame, а не матрицы
         df_feature_encoded = one_hot_encoder.fit_transform(df_feature.values.reshape(-1, 1))
@@ -24,37 +27,44 @@ class Encoder:
         df_encoded = pd.DataFrame(df_feature_encoded, columns=one_hot_encoder.get_feature_names_out())
         return df_encoded
 
-    def binaryEncoder(self, df_feature):
+    @staticmethod
+    def binaryEncoder(df_feature):
         encoder = ce.BinaryEncoder()
         df_feature_encoded = encoder.fit_transform(df_feature)
         return df_feature_encoded
 
-    def helmertEncoder(self, df_feature):
+    @staticmethod
+    def helmertEncoder(df_feature):
         encoder = ce.HelmertEncoder()
         df_feature_encoded = encoder.fit_transform(df_feature)
         return df_feature_encoded
 
-    def backwardDifferenceEncoder(self, df_feature):
+    @staticmethod
+    def backwardDifferenceEncoder(df_feature):
         encoder = ce.BackwardDifferenceEncoder()
         df_feature_encoded = encoder.fit_transform(df_feature)
         return df_feature_encoded
 
-    def hashingEncoder(self, df_feature, n_components=8):
+    @staticmethod
+    def hashingEncoder(df_feature, n_components=8):
         encoder = ce.HashingEncoder(n_components=n_components)
         df_feature_encoded = encoder.fit_transform(df_feature)
         return df_feature_encoded
 
-    def targetEncoder(self, df_feature, df_target):
+    @staticmethod
+    def targetEncoder(df_feature, df_target):
         encoder = ce.TargetEncoder()
         df_feature_encoded = encoder.fit_transform(df_feature, df_target)
         return df_feature_encoded
 
-    def leaveOneOutEncoder(self, df_feature, df_target):
+    @staticmethod
+    def leaveOneOutEncoder(df_feature, df_target):
         encoder = ce.LeaveOneOutEncoder()
         df_feature_encoded = encoder.fit_transform(df_feature, df_target)
         return df_feature_encoded
 
-    def jamesSteinEncoder(self, df_feature, df_target):
+    @staticmethod
+    def jamesSteinEncoder(df_feature, df_target):
         encoder = ce.JamesSteinEncoder()
         df_feature_encoded = encoder.fit_transform(df_feature, df_target)
         return df_feature_encoded
